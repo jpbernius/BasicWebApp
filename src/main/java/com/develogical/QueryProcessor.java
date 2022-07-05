4890a30cfe4dedd3accf62a673a493d7d19d00f9
Jan Philip Bernius <janphilip@bernius.net>
2022-07-05 10:59:04 +0200

    test

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,10 +9,10 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
         if (query.toLowerCase().contains("which of the following numbers is the largest")) {
-            var segments = query.toLowerCase().split(":");
-            var num1 = Integer.parseInt(segments[1]);
-            var num2 = Integer.parseInt(segments[2]);
-            return (num1 < num2) ? num2 : num1;
+            String[] segments = query.toLowerCase().split(":");
+            int num1 = Integer.parseInt(segments[1]);
+            int num2 = Integer.parseInt(segments[2]);
+            return String.valueOf((num1 < num2) ? num2 : num1);
         }
         return "";
     }
