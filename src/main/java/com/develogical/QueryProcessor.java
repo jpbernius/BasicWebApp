fdefc9c3de0830f75620b9df8fd977efaa3b46b7
Jan Philip Bernius <janphilip@bernius.net>
2022-07-05 10:56:12 +0200

    largest

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,6 +8,12 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        if (query.toLowerCase().contains("which of the following numbers is the largest")) {
+            var segments = query.toLowerCase().split(":")
+            var num1 = Integer.parseInt(segments[1]);
+            var num2 = Integer.parseInt(segments[2]);
+            return (num1 < num2) ? num2 : num1;
+        }
         return "";
     }
 }
